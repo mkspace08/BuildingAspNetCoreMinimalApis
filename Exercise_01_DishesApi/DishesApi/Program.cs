e9bb0480c7a17d27ddbaea949423c53388c91476
Mirosław Kokociński <mkspace08@gmail.com>
2025-12-05 18:27:17 +0100

    New dishes endopint added. IIES congig disabled, run docker diabled for now.

--- Exercise_01_DishesApi/DishesApi/Program.cs
+++ Exercise_01_DishesApi/DishesApi/Program.cs
@@ -20,17 +20,38 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/dishes", async (DishesDbContext dishesDbContext) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return dishesDbContext.Dishes.ToListAsync();
+});
+
+// Get first dish that matches the provided name (case-insensitive, substring match)
+app.MapGet("/dishes/{name}", async (string name, DishesDbContext dishesDbContext) =>
+{
+    var lowered = name.ToLower();
+    var dish = await dishesDbContext.Dishes
+        .FirstOrDefaultAsync(d => d.Name.ToLower().Contains(lowered));
+
+    return dish is not null ? Results.Ok(dish) : Results.NotFound();
+});
+
+app.MapGet("/dishes/{id:guid}", async (Guid id, DishesDbContext dishesDbContext) =>
+{
+    var dish = await dishesDbContext.Dishes
+        //.Include(d => d.Ingredients)
+        .FirstOrDefaultAsync(d => d.Id == id);
+
+    return dish is not null ? Results.Ok(dish) : Results.NotFound();
+});
+
+app.MapGet("/dishes/{dishId}/ingredients", async (Guid dishId, DishesDbContext dishesDbContext) =>
+{
+    var dish = await dishesDbContext.Dishes
+        .Include(d => d.Ingredients)
+        .FirstOrDefaultAsync(d => d.Id == dishId);
+    if (dish is null)
+        return Results.NotFound();
+    return Results.Ok(dish.Ingredients);
 });
 
 // recreate & migrate the database on each run, for demo purposes
@@ -43,7 +64,3 @@ using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().Create
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
